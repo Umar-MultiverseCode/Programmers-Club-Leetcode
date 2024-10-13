68f17ad1c867fa77374495d902c322cf7b5f29ca
Umar Mulla <139649458+Umar-MultiverseCode@users.noreply.github.com>
2024-10-13 15:51:29 +0530

    q2089_FindTargetIndicesAfterSortingArray

--- OCTOBER/RANDOM_QUESTIONS/q2089_FindTargetIndicesAfterSortingArray.java
+++ OCTOBER/RANDOM_QUESTIONS/q2089_FindTargetIndicesAfterSortingArray.java
@@ -1,12 +1,30 @@
 package RANDOM_QUESTIONS;
 
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // https://leetcode.com/problems/find-target-indices-after-sorting-array/description/
 // to do on sunday
 public class q2089_FindTargetIndicesAfterSortingArray {
     public static void main(String[] args) {
 
+        int []a={1,2,5,2,3};
+        System.out.println(targetIndices(a,3));
+
     }
-//    static List<Integer> targetIndices(int[] nums, int target) {
-//
-//    }
+    static List<Integer> targetIndices(int[] nums, int target) {
+        Arrays.sort(nums);
+        List<Integer> result=new ArrayList<>();
+
+        for(int i=0;i<nums.length;i++){
+
+            if(nums[i]==target){
+                result.add(i);
+            }
+
+        }
+        return result;
+    }
 }
