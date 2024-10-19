9d53b89cf33f181b33ae12f4340a28ea3fc6f8f7
Umar Mulla <139649458+Umar-MultiverseCode@users.noreply.github.com>
2024-10-19 22:53:14 +0530

    update

---
+++ OCTOBER/RANDOM_QUESTIONS/q1051_HeightChecker.java
@@ -0,0 +1,15 @@
+package RANDOM_QUESTIONS;
+
+import java.util.Arrays;
+
+public class q1051_HeightChecker {
+    public static void main(String[] args) {
+
+    }
+
+//    static int heightChecker(int[] heights) {
+//
+//        int count = 0;
+//    }
+}
+
